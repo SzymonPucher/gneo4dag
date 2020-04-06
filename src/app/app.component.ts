aceff55fa6ad8a6f291ef2d82c50e3a1afa66db5
Szymon Pucher <sajmontube+gitlab@gmail.com>
2020-04-06 07:33:17 +0200

    Graph show example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularNeo4jService } from 'angular-neo4j';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  
   title = 'n4jang';
+
+  before: Array<any>;
+  after: Array<any>;
+  the_one: string;
+
+
+  constructor(private neo4j: AngularNeo4jService) {
+  const url = 'bolt://localhost:7687';
+  const username = 'neo4j';
+  const password = 'nela';
+  const encrypted = false;
+
+  this.neo4j
+      .connect(
+        url,
+        username,
+        password,
+        encrypted
+      )
+      .then(driver => {
+        if (driver) {
+          console.log(`Successfully connected to ${url}`);
+        }
+      });
+
+    this.before = ['prev'];
+    this.after = ['after'];
+    this.the_one = 'this_one';
+  }
+
+  runQuery(value: string) { 
+    const query = 'MATCH (n {name: $name})-[r]-(n2) RETURN n.name, n, r, n2';
+    this.the_one = value;
+    this.before = []; 
+    this.after = [];
+    this.neo4j.run(query, { name: value }).then(res => {
+      console.log(res);
+      res.forEach(element => {  
+        if(element[1].identity == element[2].end){
+          this.before.push(element[3].properties.name)
+        }
+        else{
+          this.after.push(element[3].properties.name)
+        }
+      });
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularNeo4jModule } from 'angular-neo4j';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShowGraphComponent } from './show-graph/show-graph.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShowGraphComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularNeo4jModule
   ],
   providers: [],
   bootstrap: [AppComponent]
